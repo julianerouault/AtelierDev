ea061e159e49e0ebbf24e70737c0ed94abca6eaa
Unknown <rouault.juliane@gmail.com>
2018-07-10 11:14:03 +0200

    a prendre

--- index.php
+++ index.php
@@ -5,12 +5,7 @@ require_once 'model/database.php';
 getHeader("Accueil");
 ?>
 
-<section class="container">
-
-  <div class="row">
-
-    <div class="col-lg-6">
-      <form style="background-color: red;" action="index.php" method="post" class="form-signin">
+      <form action="index.php" method="post" class="form-signin">
         <h1 class="h3 mb-3 font-weight-normal">CONNEXION</h1>
         <label for="inputEmail" class="sr-only">Email</label>
         <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
@@ -23,47 +18,28 @@ getHeader("Accueil");
         </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">se connecter</button>
         </form>
-      </div>
 
-    <div class="col-lg-6">
-      <form style="background-color: grey;"action="admin/Inscription.php" method="post" class="form-signin">
-        <h1 class="h3 mb-3 font-weight-normal">INSCRIPTION</h1>
 
-        <label for="inputEmail" class="sr-only">Email</label>
-        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
 
-        <label for="inputPassword" class="sr-only">Mot de passe</label>
-        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
 
-        <label for="role">vous êtes ?</label><br />
-               <select name="role" id="role">
-                   <option value="etudiant">Etudiant</option>
-                   <option value="entreprise">Professionnel</option>
-               </select>
+        <form style="float: right;" action="admin/Inscription.php" method="post" class="form-signin">
+          <h1 class="h3 mb-3 font-weight-normal">INSCRIPTION</h1>
+          <label for="inputEmail" class="sr-only">Email</label>
+          <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+          <label for="inputPassword" class="sr-only">Mot de passe</label>
+          <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
 
-          <div class="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me"> Se souvenir de moi
-            </label>
+          <label for="role">Vous êtes ?</label><br />
+          <select name="role" id="role">
+            <option value="etudiant">Etudiant</option>
+            <option value="professionnel">Professionnel</option>
+          </select>
 
-          </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">s'inscrire</button>
-      </form>
-    </div>
-
-<form style="float: right;" action="admin/Inscription.php" method="post" class="form-signin">
-    <h1 class="h3 mb-3 font-weight-normal">INSCRIPTION</h1>
-    <label for="inputEmail" class="sr-only">Email</label>
-    <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
-    <label for="inputPassword" class="sr-only">Mot de passe</label>
-    <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
-    <div class="checkbox mb-3">
+        <div class="checkbox mb-3">
         <label>
             <input type="checkbox" value="remember-me"> Se souvenir de moi
         </label>
-    </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">s'inscrire</button>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">s'inscrire</button>
 </form>
 </section>
-
-
