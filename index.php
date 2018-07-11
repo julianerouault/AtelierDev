b841c802b9088937c6fd65ba8a5461dc0cfc6f9d
Monarch09 <nicoleau.c44@gmail.com>
2018-07-11 09:39:40 +0200

    Modif index - fix

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ getHeader("Accueil");
               <p>L'email est déjà utilisé</p>
             <?php endif; ?>
           <?php endif; ?>
+          <h1 class="h3 mb-3 font-weight-normal" style="color: #253745">INSCRIPTION</h1>
           <label for="inputEmail" class="sr-only">Email</label>
           <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
           <label for="inputPassword" class="sr-only">Mot de passe</label>
